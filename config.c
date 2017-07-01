e2ac9f761d3b9643f6bb79e179cc23d5f92f195d
jocelynshen <jocelynshenbb@gmail.com>
2017-07-01 16:33:11 -0700

    drive train works

--- config.c
+++ config.c
@@ -2,10 +2,10 @@
 #define CONFIG_C
 
 void move(int V, int H, int X){
-  motorReq[M_WHEEL_L1] = BOUND(V + H, -127, 127);
-  motorReq[M_WHEEL_L2] = BOUND(V + H, -127, 127);
-  motorReq[M_WHEEL_R1] = BOUND(V - H, -127, 127);
-  motorReq[M_WHEEL_R2] = BOUND(V - H, -127, 127);
+  motor[M_WHEEL_L1] = BOUND(V + H, -127, 127);
+  motor[M_WHEEL_L2] = BOUND(V + H, -127, 127);
+  motor[M_WHEEL_R1] = BOUND(V - H, -127, 127);
+  motor[M_WHEEL_R2] = BOUND(V - H, -127, 127);
 }
 
 #endif
