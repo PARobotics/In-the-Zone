bab0bdd91f348b65a11c6214611f79d4657e6fa9
jeffreyshen19 <jshen@andover.edu>
2017-07-10 08:31:47 -0700

    added slew rate control

--- config.c
+++ config.c
@@ -34,8 +34,8 @@ void lcdGenerateMessage(){
 */
 
 //Slew Rate
-#define USE_SLEW  0 //Disable if slew interferes with move functions or slows robot down
-int MOTOR_SLEW[MOTOR_NUM] = {255, 40, 40, 40, 40, 255, 255, 255, 255, 255};
+#define USE_SLEW  1 //Disable if slew interferes with move functions or slows robot down
+int MOTOR_SLEW[MOTOR_NUM] = {255, 40, 40, 255, 255, 255, 255, 40, 40, 255};
 
 #define USE_MONITOR 0//Toggles the monitor task (Necessary for move functions, should be disabled otherwise)
 
@@ -66,10 +66,10 @@ void getWheelVal(){
 #define BAILOUT_BUTTON Btn7D
 
 void move(int V, int H, int X){
-  motor[M_WHEEL_L1] = BOUND(V + H, -127, 127);
-  motor[M_WHEEL_L2] = BOUND(V + H, -127, 127);
-  motor[M_WHEEL_R1] = BOUND(V - H, -127, 127);
-  motor[M_WHEEL_R2] = BOUND(V - H, -127, 127);
+  motorReq[M_WHEEL_L1] = BOUND(V + H, -127, 127);
+  motorReq[M_WHEEL_L2] = BOUND(V + H, -127, 127);
+  motorReq[M_WHEEL_R1] = BOUND(V - H, -127, 127);
+  motorReq[M_WHEEL_R2] = BOUND(V - H, -127, 127);
 }
 
 #endif
