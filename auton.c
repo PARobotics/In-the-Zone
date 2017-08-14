4508b06c9314ab99cd433e63a46c24dd7eb1a877
jeffreyshen19 <jshen@andover.edu>
2017-08-14 13:47:00 -0700

    updated deploy cone lift

--- auton.c
+++ auton.c
@@ -4,13 +4,14 @@
 void deployConeLift(){ //Swings cone lift out into ready position
 
   //Stage 1: Move the first lift up to clear out of the way
+  CONE_LIFT_COMMAND = MANUAL;
   moveLiftTo(440, 0, 1000);
-  CONE_LIFT_COMMAND = HOLD;
 
   //Stage 2: Rotate the turntable
   moveTurntableToFront();
 
   //Stage 3: Deploy the second lift
+  CONE_LIFT_COMMAND = MANUAL;
   moveLiftTo(975, 3170, 2000);
   openClaw();
 
