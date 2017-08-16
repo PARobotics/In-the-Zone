d23c8ce0e55867d4465cf0a00fb5a1aff64a8cc8
Jeffrey Shen <jshen@andover.edu>
2017-08-15 17:01:57 -0700

    updated constants

--- main.h
+++ main.h
@@ -46,7 +46,7 @@ void deployConeLift();
 #define CONE_LIFT2_MAX_V 127
 #define CONE_LIFT2_MIN_V -127
 #define CONE_LIFT2_DEFAULT_V 0
-#define CONE_LIFT2_KX -0.5
+#define CONE_LIFT2_KX -1.5
 #define CONE_LIFT2_KV -0.02
 
 
