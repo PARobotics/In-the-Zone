ca5954803d0a404b67e79ea0cd8a65d707e0b72c
jeffreyshen19 <jshen@andover.edu>
2017-08-15 18:48:37 -0700

    not always holding

--- cone_lift.c
+++ cone_lift.c
@@ -259,6 +259,7 @@ task coneLiftTask(){ //Controls the position of the lift continuously
     else if(CONE_LIFT_COMMAND == PRESET){
       currentlyCarrying = 0;
       moveLiftTo(firstLiftVal, secondLiftVal, 3000);
+      CONE_LIFT_COMMAND = STOP;
     }
     else if(CONE_LIFT_COMMAND == MANUAL){
       currentlyCarrying = 0;
